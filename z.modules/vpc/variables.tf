ef47d615613bfa648e72a670b23ecc058bb399d0
venu-test-at-458658971794 <venugopalreddy.pagidi@ibexlabs.com>
2023-09-20 08:50:57 +0530

    third commit

---
+++ z.modules/vpc/variables.tf
@@ -0,0 +1,38 @@
+variable "DMK" {
+  type    = string
+  default = null
+}
+
+variable "env" {
+  type    = string
+  default = null
+}
+
+variable "region" {
+  type    = string
+  default = "us-west-1"
+}
+
+variable "zones" {
+  type    = list(string)
+  default = ["a", "b", "c"]
+}
+
+variable "vpc_cidr" {
+  type    = string
+  default = null
+}
+
+variable "public_subnet_cidr_blocks" {
+  type    = map(string)
+  default = {}
+}
+
+variable "private_subnet_cidr_blocks" {
+  type    = map(string)
+  default = {}
+}
+variable "db_private_subnet_cidr_blocks" {
+  type    = map(string)
+  default = {}
+}
